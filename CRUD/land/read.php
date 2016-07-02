797b51cdd6c45c0e06c679d48a1bf9bf1a25ae7d
wexnox <lasseronnlie@hotmail.com>
2016-07-02 04:17:47 +0200

    add todo

--- CRUD/land/read.php
+++ CRUD/land/read.php
@@ -18,7 +18,7 @@ if ( null==$id ) {
 } else {
     $pdo = Database::connect();
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "SELECT * FROM flytyper where id = ?";
+    $sql = "SELECT * FROM land where id = ?";
     $q = $pdo->prepare($sql);
     $q->execute(array($id));
     $data = $q->fetch(PDO::FETCH_ASSOC);
@@ -32,16 +32,8 @@ if ( null==$id ) {
             <form class="form" role="form">
                 <div class="form-horizontal">
                     <div class="form-group">
-                        <label>Registrert Model:</label>
-                        <?php echo $data['model'];?>
-                    </div>
-                    <div class="form-group">
-                        <label>Registrert Navn:</label>
+                        <label>Registrert Land:</label>
                         <?php echo $data['navn'];?>
-                    </div>
-                    <div class="form-group">
-                        <label>Registrert antallseter:</label>
-                        <?php echo $data['seter'];?>
                     </div>
                     <a class="btn btn-default" href="index.php">Back</a>
                 </div>
