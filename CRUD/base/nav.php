367f78993baa85a241ca6bde07559e0fc6fb35ad
wexnox <lasseronnlie@hotmail.com>
2016-07-05 21:50:12 +0200

    Resolve nav error

--- CRUD/base/nav.php
+++ CRUD/base/nav.php
@@ -4,15 +4,8 @@
  * User: wexnox
  * Date: 08/06/2016
  * Time: 17:12
- *
- * <li><a href="#">Reise</a></li>
- * <li><a href="#">Flyrute</a></li>
- * <li><a href="#">Flybilettbestilling</a></li>
- * <li><a href="#">Prisliste</a></li>
- *
- *
- *
  */
+$url = 'http://homestead.app/CRUD';
 ?>
 <nav class="navbar navbar-default">
     <div class="container">
@@ -24,26 +17,33 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="../index.php">Home</a>
+                <a class="navbar-brand" href="<?php echo "$url";?>/index.php">Home</a>
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     <li class="dropdown">
                         <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Fly<span class="caret"></span></a>
                         <ul class="dropdown-menu">
-                            <li><a href="fly/index.php">Fly</a></li>
-                            <li><a href="flytype/index.php">Flytype</a></li>
+                            <li><a href="<?php echo "$url";?>/fly/index.php">Fly</a></li>
+                            <li><a href="<?php echo "$url";?>/flytype/index.php">Flytype</a></li>
                             <li role="separator" class="divider"></li>
-                            <li><a href="land/index.php">Land</a></li>
-                            <li><a href="flyplasser/index.php">Flyplass</a></li>
+                            <li><a href="<?php echo "$url";?>/land/index.php">Land</a></li>
+                            <li><a href="<?php echo "$url";?>/flyplasser/index.php">Flyplass</a></li>
                         </ul>
                     </li>
-                </ul>
-                <ul class="nav navbar-nav">
                     <li class="dropdown">
                         <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Kunde<span class="caret"></span></a>
                         <ul class="dropdown-menu">
-                            <li><a href="kunde/index.php">Kunde</a></li>
+                            <li><a href="<?php echo "$url";?>/kunde/index.php">Kunde</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Reise<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="<?php echo "$url";?>">Reise</a></li>
+                            <li><a href="<?php echo "$url";?>">Flyrute</a></li>
+                            <li><a href="<?php echo "$url";?>">Flybilettbestilling</a></li>
+                            <li><a href="<?php echo "$url";?>">Prisliste</a></li>
                         </ul>
                     </li>
                 </ul>
@@ -58,10 +58,10 @@
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">User Account<span class="caret"></span></a>
                         <ul class="dropdown-menu">
-                            <li><a href="admin/index.php">Edit User Settings</a></li>
-                            <li><a href="admin/chpass.php">Change password</a></li>
+                            <li><a href="<?php echo "$url";?>/admin/index.php">Edit User Settings</a></li>
+                            <li><a href="<?php echo "$url";?>/admin/chpass.php">Change password</a></li>
                             <li role="separator" class="divider"></li>
-                            <li><a href="#admin/logout.php">logout</a></li>
+                            <li><a href="<?php echo "$url";?>/admin/logout.php">logout</a></li>
                         </ul>
                     </li>
                 </ul>
