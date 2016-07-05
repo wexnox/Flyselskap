06552297e5c10dfcf5b53e7a998ddfe08c119466
wexnox <lasseronnlie@hotmail.com>
2016-07-05 14:30:30 +0200

    jquery

--- CRUD/base/footer.php
+++ CRUD/base/footer.php
@@ -12,14 +12,15 @@
 </div>
 </div>
 <!--Script dump-->
-<script src="assets/js/jquery-1.12.3.js"></script>
-<script src="assets/js/jquery-ui.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+
+
 <script type="text/javascript" src="assets/js/jquery.timepicker.min.js"></script>
 <script src="assets/js/modernizr-2.8.3.min.js"></script>
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 <!--    Bootstrap ends-->
 <!--Script dump ends-->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+
 </body>
 </html>
