4b5a167d0b57addf150fa5bcc78c5a5a00e90f02
wexnox <lasseronnlie@hotmail.com>
2016-07-06 01:26:25 +0200

    dupe check implemented

--- CRUD/fly/create.php
+++ CRUD/fly/create.php
@@ -6,77 +6,73 @@
  * Date: 26/06/2016
  * Time: 17:39
  * TODO: trenger JavaScript validering
+ * TODO: Her må jeg bytte ut rowCount med COUNT(*) og fetchcolumn se flytype\update.php
  */
 include ('../base/head.php');
 include ('../base/nav.php');
 require '../base/db-connection.php';
 if ( !empty($_POST)) {
-    $modelError = null;
-    $navnError = null;
-    $seterError = null;
+    $flytype_idError = null;
+    $kodeError = null;
 
-    $model = $_POST['model'];
-    $navn = $_POST['navn'];
-    $seter = $_POST['seter'];
+    $flytype_id = $_POST['flytype_id'];
+    $kode = $_POST['kode'];
+
 
     $valid = true;
-    if (empty($model)) {
-        $modelError = 'Fyll ut Model';
+    if (empty($flytype_id)) {
+        $flytype_idError = 'Fyll ut flytype';
         $valid = false;
     }
-    if (empty($navn)) {
-        $navnError = 'Fyll ut Navn';
-        $valid = false;
-    }
-    if (empty($seter)) {
-        $seterError = 'Fyll ut Max antallseter';
+    if (empty($kode)) {
+        $kodeError = 'Fyll ut flykode';
         $valid = false;
     } else {
         $pdo = Database::connect();
-        $count= $pdo->prepare('SELECT model FROM flytyper WHERE model=:model');
-        $count ->bindParam(":model",$model);
-        $count->execute();
-        $no=$count->rowCount();
-        if ($no > 0){
-            $valid =false;
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = $pdo->prepare("SELECT COUNT(*) AS `total` FROM fly WHERE kode = :kode");
+        $sql->execute(array(':kode' => $kode));
+        $result = $sql->fetchObject();
+        if ($result->total > 0)
+        {
+            echo '<div class="container">';
+            echo '<div class="alert alert-danger">';
+            echo '<p class="lead">Fly: <strong>' . $kode. '</strong> er allerede i bruk.<p>';
+            echo '</div></div>';
         }
         else{
-            $pdo = Database::connect();
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $sql = "INSERT INTO flytyper(model,navn,seter) VALUES(?, ?, ?)";
-            $q = $pdo->prepare($sql);
-            $q->execute(array($model, $navn, $seter));
-            Database::disconnect();
-            header("Location:index.php");
+            if ($valid) {
+                $pdo = Database::connect();
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $sql = "INSERT INTO fly(flytype_id, kode) VALUES(?, ?)";
+                $q = $pdo->prepare($sql);
+                $q->execute(array($flytype_id, $kode));
+                Database::disconnect();
+                header("Location:index.php");
+            }
         }
     }
 }
+include ('listeboks-flytype.php');
 ?>
     <div class="container">
+        <h3>Lag en ny flytype</h3>
         <div class="row">
             <div class="col-md-4">
-                <h3>Lag en ny flytype</h3>
-
-                <form class="form-horizontal" action="create.php" method="post">
-                    <div class="form-group <?php echo !empty($modelError)?'error':'';?>">
-                        <label for="model">Model:</label>
-                        <input class="form-control" name="model" id="model" type="text"  placeholder="Model" value="<?php echo !empty($model)?$model:'';?>">
-                        <?php if (!empty($modelError)): ?>
-                            <span class="help-inline"><?php echo $modelError;?></span>
-                        <?php endif; ?>
+                <form class="form-horizontal" role="form" action="create.php" method="post">
+                    <div class="form-group <?php echo !empty($flytype_idError)?'error':'';?>">
+                        <label for="flytype_id">Flytype ID:</label>
+                        <select class="form-control" name="flytype_id" id="flytype_id">
+                            <?php foreach ($flytype as $row): ?>
+                                <option><?=$row["id"].$row["model"].$row["navn"]?></option>
+                            <?php endforeach ?>
+                        </select>
                     </div>
-                    <div class="form-group <?php echo !empty($navnError)?'error':'';?>">
-                        <label for="name">Navn:</label>
-                        <input class="form-control" name="navn" id="name" type="text" placeholder="Navn" value="<?php echo !empty($navn)?$navn:'';?>">
-                        <?php if (!empty($navnError)): ?>
-                            <span class="help-inline"><?php echo $navnError;?></span>
-                        <?php endif;?>
-                    </div>
-                    <div class="form-group <?php echo !empty($seterError)?'error':'';?>">
-                        <label for="seter">Maks antallseter:</label>
-                        <input class="form-control" name="seter" id="seter" type="text"  placeholder="Antallseter" value="<?php echo !empty($seter)?$seter:'';?>">
-                        <?php if (!empty($seterError)): ?>
-                            <span class="help-inline"><?php echo $seterError;?></span>
+                    <div class="form-group <?php echo !empty($kodeError)?'error':'';?>">
+                        <label for="kode">Fly Kode</label>
+                        <input class="form-control" name="kode" id="kode" type="text" placeholder="Fyll ut flykode" value="<?php echo !empty($kode)?$kode:'';?>">
+                        <?php if (!empty($kodeError)): ?>
+                            <span class="help-inline"><?php echo $kodeError;?></span>
                         <?php endif;?>
                     </div>
                     <button type="submit" class="btn btn-success">Create</button>
